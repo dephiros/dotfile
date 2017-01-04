f5c8813ee30b3dfb4ee9f0f22e3728af416985c1
An Nguyen <an.nguyen@zonarsystems.com>
2017-01-04 13:43:53 -0800

    add better default, magit and ido everywhere

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,8 +15,53 @@
        (set-face-attribute 'default nil :font "Source Code Pro-13"))
       ((member "Inconsolata" (font-family-list))
        (set-face-attribute 'default nil :font "Inconsolata-14")))
+; set up packages
+(require 'package)
+;; list of packages
+(defvar my-packages '(better-defaults ido-ubiquitous magit smex))
+
+(add-to-list 'package-archives
+     '("marmalade" . "https://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+     '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package my-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; set up ido-ubiquitous
+(ido-mode 1)
+(ido-everywhere 1)
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
+
+;; smex ido in M-x
+(require 'smex) ; Not needed if you use package.el
+;;; bind M-x to use smex instead of default emacs
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;;;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 ; finally start emacs server
 (require 'server)
 (unless (server-running-p)
     (server-start))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (smex magit ido-ubiquitous better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
