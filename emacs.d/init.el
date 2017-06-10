f8af057ebc7d0401202ead8c9bddd3c892a1d1fe
An Nguyen <dephiros@gmail.com>
2017-06-10 09:47:15 -0700

    change theme to spacemacs to get link underline

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,17 +22,3 @@
                    user-emacs-directory))
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (magit use-package evil-org evil-leader base16-theme auto-compile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
