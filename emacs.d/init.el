c68571492f05daf149d32b76375ec9c8bc5ceb06
An Nguyen <dephiros@gmail.com>
2017-06-09 19:12:35 -0700

    initial emacs set up

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,90 +5,20 @@
 ;;
 
 ;; load Org-mode from source when the ORG_HOME environment variable is set
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (when (getenv "ORG_HOME")
   (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
     (when (file-directory-p org-lisp-dir)
       (add-to-list 'load-path org-lisp-dir)
       (require 'org))))
+(org-babel-load-file
+(expand-file-name "settings.org"
+                   user-emacs-directory))
 
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(setq use-package-verbose t)
-(setq use-package-always-ensure t)
-(require 'use-package)
-(use-package auto-compile
-  :config (auto-compile-on-load-mode))
-(setq load-prefer-newer t)
-
-(require 'diminish)
-(require 'bind-key)
-
-(use-package evil
-  :init
-  :config
-  (require 'evil)
-  (evil-mode t))
-
-(use-package base16-theme
-  :config
-  (load-theme `base16-tomorrow-night t))
-
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
-
-(use-package flycheck-title
-  :config
-  (with-eval-after-load 'flycheck
-    (flycheck-title-mode)))
-
-(use-package flycheck-color-mode-line
-  :config
-  (eval-after-load "flycheck"
-  '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
-
-(use-package flx-ido
-  :config
-  (require 'flx-ido)
-  (ido-mode 1)
-  (ido-everywhere 1)
-  (flx-ido-mode 1)
-  ;; disable ido faces to see flx highlight
-  (setq ido-enable-flex-matching t)
-  (setq ido-use-faces nil))
-
-(use-package magit)
-
-;; language and stuff
-(use-package elixir-mode)
-(use-package web-mode)
-(use-package vue-mode)
-(use-package markdown-mode)
-
-
-;; start emacs server
-(require 'server)
-(unless (server-running-p)
-  (server-start))
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (vue-mode web-mode flycheck markdown-mode evil use-package auto-compile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
