44bb9658bfe1fb10c445dd033eedfabaab6da29b
An Nguyen <dephiros@gmail.com>
2018-02-19 10:00:40 -0800

    add org wiki and helm

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -17,6 +17,16 @@
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
 
+;; Key bindings
+
+(when *is-a-mac* ;; mac specific settings
+  (setq mac-option-key-is-meta t)
+  (setq mac-command-key-is-meta nil)
+  (setq mac-command-modifier nil)
+  (setq  mac-option-modifier 'meta)
+  (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
+)
+
 
 ;;----------------------------------------------------------------------------
 ;; visual settings
@@ -28,12 +38,12 @@
 
 (tool-bar-mode -1)                      ; no tool bar with icons
 (scroll-bar-mode -1)                    ; no scroll bars
-(unless (string-match "apple-darwin" system-configuration)
+(if (not *is-a-mac*)
   ;; on mac, there's always a menu bar drown, don't have it empty
   (menu-bar-mode -1))
 
 ;; choose your own fonts, in a system dependant way
-(if (string-match "apple-darwin" system-configuration)
+(if *is-a-mac*
     (set-face-font 'default "Monaco-14")
   (set-face-font 'default "Monospace-14"))
 
@@ -90,50 +100,62 @@
 
 ;; this package help fuzzy matching
 (use-package flx)
-
-;; ivy replaces ido and configure fuzzy matching that rely on flx for sorting
-;; https://oremacs.com/2016/01/06/ivy-flx/
-(use-package ivy
-  :config
-  (ivy-mode 1)
-  (setq ivy-re-builders-alist
-    '((swiper . ivy--regex-plus)
-      (t . ivy--regex-fuzzy))))
-
-;; this package use ivy as a backend for text search in file
-(use-package swiper
-  :config
-  (global-set-key "\C-s" 'swiper))
-
-;; integration between ivy and projectile
-(use-package counsel-projectile
-  :config
-  (counsel-projectile-mode 1))
-
 (use-package ag)
 (use-package rg)
+
+;; helm
+(use-package helm
+  :diminish helm-mode
+  :init
+  (progn
+    (require 'helm-config)
+    (setq helm-candidate-number-limit 100)
+    ;; From https://gist.github.com/antifuchs/9238468
+    (setq helm-idle-delay 0.0 ; update fast sources immediately (doesn't).
+          helm-input-idle-delay 0.01  ; this actually updates things
+                                        ; reeeelatively quickly.
+          helm-yas-display-key-on-candidate t
+          helm-quick-update t
+          helm-M-x-requires-pattern nil
+          helm-ff-skip-boring-files t)
+    (helm-mode))
+  :bind (("C-c h" . helm-mini)
+         ("C-h a" . helm-apropos)
+         ("C-x C-f" . helm-find-files)
+         ("C-x C-b" . helm-buffers-list)
+         ("C-x b" . helm-buffers-list)
+         ("M-y" . helm-show-kill-ring)
+         ("M-x" . helm-M-x)
+         ("C-x c o" . helm-occur)
+         ("C-x c s" . helm-swoop)
+         ("C-x c y" . helm-yas-complete)
+         ("C-x c Y" . helm-yas-create-snippet-on-region)
+         ("C-x c b" . my/helm-do-grep-book-notes)
+         ("C-x c SPC" . helm-all-mark-rings)))
+(ido-mode -1) ;; Turn off ido mode in case I enabled it accidentally
+(straight-use-package
+ '(helm-rg :type git :host github :repo "microamp/helm-rg"))
+(use-package helm-projectile)
 
 (use-package exec-path-from-shell
   :init
   (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize)))
 
-;; org brain for wiki. See https://github.com/Kungsgeten/org-brain
-(use-package org-brain
-  :init
-  (setq org-brain-path "~/org/brain")
+;; org-wiki
+(use-package let-alist)
+(use-package org-wiki
+  :straight (org-wiki :type git :host github :repo "caiorss/org-wiki")
   :config
-  (setq org-id-track-globally t)
-  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
-  ; You might want to add information at the end of an entry, without visiting the file. A way to do this is to use a capture template, such as the one presented above.
-  ; (push '("b" "Brain" plain (function org-brain-goto-end)
-  ;         "* %i%?" :empty-lines 1)
-  ;       org-capture-templates)
-  (setq org-brain-visualize-default-choices 'all)
-  ;; For Evil users
-  (setq org-brain-title-max-length 12)
-  (global-set-key "\C-cb" 'org-brain-visualize)
-  (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  (require 'org-wiki)
+  (setq org-wiki-location-list
+    '(
+      "~/org/wiki"
+    ))
+
+  ;; Initialize first org-wiki-directory or default org-wiki 
+  (setq org-wiki-location (car org-wiki-location-list))
+)
 
 ;;----------------------------------------------------------------------------
 ;; Org settings
