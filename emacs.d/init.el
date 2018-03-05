85d976d68551be6c42b33c4605c6a597242b65d6
An Nguyen <an@rover.com>
2018-03-05 14:10:04 -0800

    make sure underscore is not used as subscript

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -198,7 +198,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/org/note.org"))))
+ '(org-agenda-files (quote ("~/org/note.org")))
+ '(org-export-with-sub-superscripts (quote {}))
+ '(org-use-sub-superscripts (quote {})))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
