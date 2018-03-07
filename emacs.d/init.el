09a54fa19d3954910dc82fa13aaf0d7a3d9438fb
An Nguyen <an@rover.com>
2018-03-06 16:33:50 -0800

    changes from Rover-AnNguyen.local on Tue Mar 6 16:33:50 PST 2018

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -198,7 +198,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
+ '(org-agenda-files (quote ("~/org/wiki/index.org")))
  '(org-export-with-sub-superscripts (quote {}))
  '(org-startup-with-inline-images t)
  '(org-use-sub-superscripts (quote {})))
