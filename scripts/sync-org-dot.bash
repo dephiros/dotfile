f622a2e4117e295fc6aa4b6a6ddbaaab3b48f0f5
An Nguyen <an@rover.com>
2018-03-12 09:03:28 -0700

    small script modification

--- scripts/sync-org-dot.bash
+++ scripts/sync-org-dot.bash
@@ -2,6 +2,7 @@
 DOTFILES=$HOME/dotfiles
 SCRIPTS=$DOTFILES/scripts
 cd $HOME/org
+emacs wiki/index.org -l ~/.emacs.d/init.el --batch -f org-html-export-to-html --kill
 $SCRIPTS/git-sync
 cd $DOTFILES
 $SCRIPTS/git-sync
