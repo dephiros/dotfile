3834d250d37549d176803c4c8de0916b7783fd86
An Nguyen <an@rover.com>
2019-11-14 11:26:22 -0800

    add new fh and fd to find directory and history

--- shellrc.sh
+++ shellrc.sh
@@ -54,8 +54,17 @@ export EDITOR=vim
 # fzf
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-bind -x '"\C-p": vim $(fzf);'
-
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+# fh - search in your command history and execute selected command
+fh() {
+  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
 
 # vscode to vscode-insiders
 alias code=code-insiders
