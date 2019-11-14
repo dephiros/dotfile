f4a91ada74a3a77155e60cc4239c8e7a1a973b40
An Nguyen <an@rover.com>
2019-11-14 11:43:13 -0800

    control p now respect .gitignore files

--- shellrc.sh
+++ shellrc.sh
@@ -52,7 +52,7 @@ alias vi=vim
 export EDITOR=vim
 
 # fzf
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # fd - cd to selected directory
 fd() {
