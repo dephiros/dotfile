75ee45ef2a49480e9d67f5f80d2b3e7df24a9c08
An Nguyen <dephiros@gmail.com>
2021-04-26 17:18:41 -0700

    Make sure bars are gone and ignore transient

--- dots/emacs.d/init.el
+++ dots/emacs.d/init.el
@@ -1,6 +1,6 @@
 ;; simplify things by removing some bars
-(menu-bar-mode nil)
-(tool-bar-mode nil)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
 ;; set display of tab to 2 col
 (setq tab-width nil)
 (setq lisp-indent-offset 2)
@@ -31,7 +31,7 @@
 ;; extra theme
 (use-package zenburn-theme)
 (use-package gruvbox-theme
-  :config (load-theme 'gruvbox-dark-medium))
+  :config (load-theme 'gruvbox-dark-medium t))
 
 (use-package editorconfig
   :ensure t
@@ -42,18 +42,3 @@
 
 ;; Start emacs server in the background
 (server-start)
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-  '(custom-safe-themes
-     '("7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
