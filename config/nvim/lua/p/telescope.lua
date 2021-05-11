ac32162f6a2acc44686669ed9b33b3625b99487d
An Nguyen <dephiros@gmail.com>
2021-05-11 15:41:43 -0700

    Add mapping for buffer search

--- config/nvim/lua/p/telescope.lua
+++ config/nvim/lua/p/telescope.lua
@@ -14,8 +14,10 @@ M.setup = function ()
 	utils.map("n", "<leader>fr", [[<cmd>Telescope registers<cr>]])
 	utils.map("n", "<leader>fd", [[<cmd>lua require('p/telescope').find_dots()<cr>]])
 	utils.map("n", "<leader>fg", [[<cmd>Telescope live_grep<cr>]])
+	utils.map("n", "<leader>fs", [[<cmd>Telescope current_buffer_fuzzy_find<cr>]])
 	utils.map("n", "<leader><leader>", [[<cmd>Telescope find_files<cr>]])
-	utils.map("n", "<leader>bb", [[<cmd>Telescope buffers<cr>]])
+	utils.map("n", "<leader>,", [[<cmd>Telescope buffers<cr>]])
+	utils.map("n", "<leader>fb", [[<cmd>Telescope buffers<cr>]])
 end
 
 return M
