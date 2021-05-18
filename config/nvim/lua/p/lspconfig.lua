a93eb326eb07e657cfaa7d8cb66079b612f1847f
An Nguyen <dephiros@gmail.com>
2021-05-17 17:26:49 -0700

    Fix remap for lsp

--- config/nvim/lua/p/lspconfig.lua
+++ config/nvim/lua/p/lspconfig.lua
@@ -5,6 +5,10 @@ function config()
 
 	--keymap
 	utils.map("n", "<leader>ga", [[<cmd>lua vim.lsp.buf.code_action()<CR>]])
+	vim.cmd('autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()')
+	utils.map("n", "g[", [[<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]])
+	utils.map("n", "g]", [[<cmd>lua vim.lsp.diagnostic.goto_next()<CR>]])
+	utils.map("n", "c-]", [[<cmd>lua vim.lsp.buf.definition()<CR>]])
 	
 	local nvim_lsp = require('lspconfig')
 	local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'svelte' }
@@ -14,11 +18,8 @@ function config()
 
 	vim.cmd([[ 
 		" Show diagnostic popup on cursor hold
-		autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()
-		nnoremap <silent> g[ <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
 		nnoremap <silent> g] <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
 		" Code navigation shortcuts
-		nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR>
 		nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
 		nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>
 		nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
@@ -27,6 +28,7 @@ function config()
 		nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
 		nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
 		nnoremap <silent> gd    <cmd>lua vim.lsp.buf.declaration()<CR>
+		echom "remapXXXX"
 	]])
 end
 
